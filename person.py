a84c732b284b49c0fcdb5a914df3641dfb307c87
lollilagging <kkenshinmorales@yahoo.com>
2021-04-26 18:07:53 +0800

    Updated Methods in customer object, Updated unittesting for customer object specifically personal and cart data manipulation

--- person.py
+++ person.py
@@ -9,7 +9,7 @@ class customer(object):
     def __init__(self, UID = "guest", password = 'guest'):
         """ Initialization of Data """
         self.login(UID, password)
-        self.cart = [{}]
+        self.cart = []
         self.password = None
         #ADD LATER THE rPRate Initialization
     
@@ -45,23 +45,29 @@ class customer(object):
         self.name = "Customer"
         self.UID = "none"
         self.expData = {"month":None, "day":None, "year":None}
+        self.password = None
         self.rp = 0
+    
+    def logout(self): 
+        self.guest()
 
     def addCart(self, prodID, amt, price): 
         """ Adds the prodId and the amount for the cart """
         found = False
-        for item in self.cart : 
-            if prodID == item['ID']:
-                item['amt'] = item['amt'] + amt
-                found = True
+
+        if len(self.cart) != 0:
+            for index in range(0,len(self.cart)-1) : 
+                if prodID == self.cart[index]['ID']:
+                    self.cart[index]['amt'] = self.cart[index]['amt'] + amt
+                    found = True
 
         if not found:
             self.cart.append({'ID':prodID, 'amt':amt, 'price':price})
 
-    def removeCart(self, prodID, amt = 1):
+    def removeCart(self, prodID):
         """ Remove An Item in The Cart """
         found = False
-        for item in range(0, len(self.cart)):
+        for item in range(0, len(self.cart)-1):
             if prodID == self.cart[item]['ID'] :
                 self.cart.pop(item)
                 found = True
@@ -80,13 +86,26 @@ class customer(object):
         self.cart.clear()
         return
 
-    def getCart(self, itemDictionary):
+    def getCart(self):
         """ returns list of dictionaries for cart
         Requires The List of Items in the shops dictionary to be viewed
         shopdictionary = {prodID : {name, price}}
         """
         return self.cart
+
+    def getItem(self, index = 0):
+        """ Return a Specific Item in Index 'index' """
+        if len(self.cart) == 0:
+            return None
+        return self.cart[index]
     
+    def getItem_str(self, prodID):
+        """ Return a Specific Item string """
+        for item in self.cart : 
+            if prodID == item['ID']:
+                return item                
+        return None
+
     def getTotal(self):
         totalPrice = 0
         for items in self.cart : 
@@ -98,6 +117,9 @@ class customer(object):
 
     def setPassword(self, newPass):
         self.password = newPass
+    
+    def setUID(self, args):
+        self.UID  = args
     
     def verifyPassword(self, input):
         return self.password == input
@@ -125,13 +147,17 @@ class customer(object):
     def getRp(self):
         return self.rp
     
-    def getUID(self) : 
+    def getUID(self): 
         return self.UID
     
-    def getName(self) : 
+    def getName(self): 
         return self.name
 
-    def getExpDate(self) : 
+    def getExpDate(self): 
         return self.expData
+
+    def __str__(self):
+        """ returns all attributes as string """
+        pass 
 
         
--- personTest.py
+++ personTest.py
@@ -1,2 +1,92 @@
-import person
+from person import customer
 import unittest
+
+class testPerson(unittest.TestCase):
+    
+    def test_login(self):
+        """ test different Parameters for login
+        Test after database is done
+        """
+        pass
+
+    def test_GuestOrLogout(self): #TESTS: guest, logout, setters, getters
+        """ TESTS IF GUEST PARAMETER, also test initialization"""
+        testUser = customer()
+        self.assertEqual(testUser.getName(),"Customer")
+        self.assertEqual(testUser.getUID(), "none")
+        self.assertEqual(testUser.getExpDate(), {"month":None, "day":None, "year":None})
+        self.assertEqual(testUser.getRp(), 0)
+        self.assertTrue(testUser.verifyPassword(None))
+
+        """ Test for Setters """
+        testUser.setName("JOHN")
+        testUser.setPassword("Testing")
+        testUser.setUID("random UID")
+        testUser.addRemoveRp(10)
+        #testUser.setExpDate() -> remove comment when expiration is implemented
+        
+
+        self.assertEqual(testUser.getName(),"JOHN")
+        self.assertTrue(testUser.verifyPassword("Testing"))
+        self.assertEqual(testUser.getUID(), "random UID")
+        self.assertEqual(testUser.getRp(), 10)
+        #self.assertNotEqual(testUser.getExpDate(), {"month":None, "day":None, "year":None})
+            #-> Remove Comment if expiration is already working
+        """ Test for logout """      
+        testUser.logout()
+        self.assertEqual(testUser.getName(),"Customer")
+        self.assertEqual(testUser.getUID(), "none")
+        self.assertEqual(testUser.getExpDate(), {"month":None, "day":None, "year":None})
+        self.assertEqual(testUser.getRp(), 0)
+        self.assertTrue(testUser.verifyPassword(None))
+
+    def test_RegisterAndRemove(self):
+        """ Create a Dummy Account and Remove it
+        to be implemented after register and database is done
+        """
+        pass
+
+    def test_addRemoveCart(self):
+        """ Tests for addition or removal of item """
+        itemDict = {
+                    "Item1": {'name': 'bread', 'price': 2.5, 'qty': None}, 
+                    "Item2": {'name': 'wine', 'price': 4, 'qty': None},
+                    "Item3": {'name': 'grapes', 'price': 1, 'qty': None}
+                    }
+                    
+                     #testing for quantity is on other object
+        """ Tests for addition of Item"""
+        testUser = customer()
+        testUser.addCart("Item1", 3, 2.5)
+        testUser.addCart("Item2", 1, 4)
+        testUser.addCart("Item3", 2, 1) 
+        
+        cartList = [{'ID':"Item1",'amt':3,'price':2.5},{'ID':"Item2",'amt':1,'price':4},{'ID':"Item3",'amt':2,'price':1}]
+
+        """ Tests for getting the Item  """
+        self.assertListEqual(testUser.getCart(), cartList, 'Check if Listing is correct') 
+        self.assertEqual(testUser.getCart()[0], cartList[0], "test for getting the list of dictionary cart")
+        self.assertEqual(itemDict[testUser.getCart()[1]['ID']], {'name': 'wine', 'price': 4, 'qty': None}, "Test for output")
+        self.assertEqual(testUser.getItem(2), cartList[2], 'test for getItem() function')
+        self.assertEqual(testUser.getItem_str("Random Thing"), None)
+        
+        """ Tests for Removing Items """
+        self.assertTrue(testUser.removeCart("Item2"), 'tests to see if search function works')
+        cartList.pop(1)
+        self.assertListEqual(testUser.getCart(), cartList, 'test if the lists still holds')
+        self.assertEqual(testUser.getCart()[1]['ID'], 'Item3', 'Tests to see if Item 3 got moved up after removal')
+        self.assertTrue(testUser.lessenProd("Item1", 1), 'test to see if item is found')
+        self.assertEqual(testUser.getItem(0)['amt'], 2, 'tests if there is subtraction')
+        
+        """ Test for Item Adding """
+        testUser.addCart("Item1", 1, 2.5)
+        self.assertListEqual(testUser.getCart(), cartList, 'test if the lists still holds')
+
+        """ Test for Clearing Cart """
+        testUser.clearCrt()
+        self.assertEqual(testUser.getCart(), [])
+
+    def test_rewardPts(self):
+        """ Test For Reward Points System 
+        Implement After Creation of Customer Database
+        """
