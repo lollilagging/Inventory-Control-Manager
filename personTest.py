24888a9fe609a5c3d179b1e4fdebc1e035d3b194
lollilagging <kkenshinmorales@yahoo.com>
2021-05-14 14:22:30 +0800

    Edited For Unittesting

--- personTest.py
+++ personTest.py
@@ -1,6 +1,14 @@
 from person import customer
 import unittest
 
+"""Limited Sample Data
+ALFKI	Alfreds Futterkiste	Maria Anders	Sales Representative	Obere Str. 57	Berlin		12209	Germany	030-0074321	030-0076545
+ANATR	Ana Trujillo Emparedados y helados	Ana Trujillo	Owner	Avda. de la Constitución 2222	México D.F.		05021	Mexico	(5) 555-4729	(5) 555-3745
+ANTON	Antonio Moreno Taquería	Antonio Moreno	Owner	Mataderos  2312	México D.F.		05023	Mexico	(5) 555-3932	
+AROUT	Around the Horn	Thomas Hardy	Sales Representative	120 Hanover Sq.	London		WA1 1DP	UK	(171) 555-7788	(171) 555-6750
+BERGS	Berglunds snabbköp	Christina Berglund	Order Administrator	Berguvsvägen  8	Luleå		S-958 22	Sweden	0921-12 34 65	0921-12 34 67
+"""
+
 class testPerson(unittest.TestCase):
     
     def test_login(self):
